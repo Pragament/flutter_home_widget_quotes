313316988c7a3d1c2fe253ade1e34508fa42fb5c
PoojithaChandragiri <poojithachandragiri903@gmail.com>
2025-02-06 19:05:17 +0530

    changes in lib folder  (#30)
    * Update quote_home_page.dart
    
    separate wallpaper button(API button default on, wallpaper button default off)
    
    * Update quotes_provider.dart
    
    if no internet,leave the previous wallpaper as it is and  leave the previous quote as it is
    
    * Update settings_helper.dart
    
    * Update tags_selection_dialog.dart
    
     ask for confirmation before deleting a tag

--- lib/helper/settings_helper.dart
+++ lib/helper/settings_helper.dart
@@ -13,38 +13,49 @@
 //     await prefs.setBool(apiQuotesKey, isEnabled);
 //   }
 // }
-
 import 'dart:convert';
-
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../models/tag_model.dart';
 
 class SettingsHelper {
   static const String apiQuotesKey = 'apiQuotesEnabled';
   static const String prefsFileName = 'quote_prefs';
   static const String tagsKey = 'savedTags';
+  static const String firstLaunchKey = 'firstLaunch';
 
+  /// Get SharedPreferences instance
   static Future<SharedPreferences> _getSharedPreferences() async {
     return await SharedPreferences.getInstance();
   }
 
-  static Future<bool> isApiQuotesEnabled() async {
+  /// Check if it's the first launch of the app
+  static Future<bool> isFirstLaunch() async {
     final prefs = await _getSharedPreferences();
-    return prefs.getBool(apiQuotesKey) ?? true; // Default to true
+    bool firstLaunch = prefs.getBool(firstLaunchKey) ?? true;
+
+    if (firstLaunch) {
+      await prefs.setBool(firstLaunchKey, false);
+    }
+    return firstLaunch;
   }
 
+  /// Check if API quotes are enabled (default: true)
+  static Future<bool> isApiQuotesEnabled() async {
+    final prefs = await _getSharedPreferences();
+    return prefs.getBool(apiQuotesKey) ?? true;
+  }
+
+  /// Enable or disable API quotes
   static Future<void> setApiQuotesEnabled(bool isEnabled) async {
     final prefs = await _getSharedPreferences();
     await prefs.setBool(apiQuotesKey, isEnabled);
   }
 
+  /// Save a list of tags to SharedPreferences
   static Future<void> saveTags(List<TagModel> tags) async {
     final prefs = await _getSharedPreferences();
-    final String tagsJsonString = json.encode(tags.map((tag) => tag.toMap()).toList());
-    print("TAgs are: $tagsJsonString");
+    final String tagsJsonString =
+        json.encode(tags.map((tag) => tag.toMap()).toList());
     await prefs.setString(tagsKey, tagsJsonString);
   }
-
 }
-
